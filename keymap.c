da95e7ebc68f9222a061fb596860f6e9b4ae7e30
stian <stian.meyer+git@gmail.com>
2021-06-30 09:59:22 +0000

    fix numbers, fixing page up/down and other various fixes

--- keymap.c
+++ keymap.c
@@ -17,6 +17,7 @@
 
 #include QMK_KEYBOARD_H
 #include "muse.h"
+//#include "keymap_norwegian.h"
 
 enum preonic_layers {
   _QWERTY,
@@ -41,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   -  |   '  |  Bksp|
+ * | s-Esc|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  Bksp|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  Å   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_SLSH,    KC_BSLS,    KC_BSPC, \
+  KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  \
   KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_GRV,  KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, KC_ENT,  \
@@ -106,21 +107,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |PrtScr| PgUp | PgDw | Home |  End |Insert|ScrlLk|NumLck|Pause |   [  |   ]  |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |Prints|
+ * |      |      |      |      |      |      |      |      |      |   -  |  /   |Prints|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Caps |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |   \  | Menu |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |RCtrl | RAlt | RGui |      |      |      |      |      |      |      |      |      |
+ * |RCtrl | RAlt | RGui |      |      |      |      |      |Prevtr| PGUP |PGDOWN| Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
-  KC_ESC, KC_PGUP, KC_PGDOWN, KC_HOME, KC_END, KC_INSERT, KC_SCROLLLOCK, KC_NUMLOCK, KC_PAUSE, KC_LBRACKET, KC_RBRACKET, KC_DELETE, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCREEN, \
+  KC_ESC, KC_PGUP, KC_PGDN, KC_HOME, KC_END, KC_INSERT, KC_SCROLLLOCK, KC_NUMLOCK, KC_PAUSE, KC_LBRACKET, KC_RBRACKET, KC_DELETE, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_SLSH, KC_PSCREEN, \
   KC_CAPSLOCK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   KC_RSHIFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLASH, KC_MENU, \
-  KC_RCTRL, KC_RALT, KC_RGUI, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  KC_RCTRL, KC_RALT, KC_RGUI, _______, _______, _______, _______, _______, KC_MPRV, KC_PGUP, KC_PGDN, KC_MNXT  \
 ),
 
 /* Raise
